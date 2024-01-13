3bbd988b263f857e421ad1606065d7d6cbfa1b15
Georgii Naumov <gmnaumov@yandex.ru>
2024-01-13 11:45:17 +0300

    feat: Added new menu item examle

--- Solution.cs
+++ Solution.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using Intermech.Bars;
+using Intermech.Interfaces.Plugins;
 
 namespace IpsSampleClientPlugin
 {
-    public class Solution
+    public class Solution : IPackage
     {
+        // Имя, отображаемое в описании модуля расширения 
+        public string Name => "Мой первый плагин IPS";
 
+        internal static IServiceProvider ipsServiseProvider;
+
+        public void Load(IServiceProvider serviceProvider)
+        {
+            ipsServiseProvider = serviceProvider;
+
+            BarManager barManager = ipsServiseProvider.GetService(typeof(BarManager)) as BarManager;
+
+            MenuBar menuBar = barManager.MenuBar;
+
+            // Имя, которое будет отображатся в главном меню IPS
+            MenuItemBase itemSamples = new MenuBarItem("MenuItemBase");
+
+            itemSamples.CommandName = "Command_sample";
+
+            menuBar.Items.Add(itemSamples);
+        }
+
+        public void Unload()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
