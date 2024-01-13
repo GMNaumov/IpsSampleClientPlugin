aed16086aeef53c7c319d0ff0dc4b69330c2277e
Georgii Naumov <gmnaumov@yandex.ru>
2024-01-13 12:30:07 +0300

    feat: Final version of adding menu item example

--- Solution.cs
+++ Solution.cs
@@ -17,14 +17,26 @@ namespace IpsSampleClientPlugin
 
             BarManager barManager = ipsServiseProvider.GetService(typeof(BarManager)) as BarManager;
 
+            // Получаем ссылку на главное меню IPS
             MenuBar menuBar = barManager.MenuBar;
 
             // Имя, которое будет отображатся в главном меню IPS
-            MenuItemBase itemSamples = new MenuBarItem("MenuItemBase");
+            MenuItemBase menuItem = new MenuBarItem("Пример элемента меню");
 
-            itemSamples.CommandName = "Command_sample";
+            // Уникальное имя элемента, по которому будет выполняться поиск методом FindMenuBar()
+            menuItem.CommandName = "MenuElementSample";
 
-            menuBar.Items.Add(itemSamples);
+            // Создаём новую кнопку элемента меню, которая будет отображаться в кастомном меню
+            MenuButtonItem menuButton = new MenuButtonItem("Пример кнопки в меню");
+
+            // Уникальное имя кнопки, по которому будет выполняться поиск 
+            menuButton.CommandName = "MenuButtonSample";
+
+            // Добавляем кнопку в элемент меню
+            menuItem.Items.Add(menuButton);
+
+            // Добавляем новое меню в главное меню IPS
+            menuBar.Items.Add(menuItem);
         }
 
         public void Unload()
