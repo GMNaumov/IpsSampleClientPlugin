95cb86031095b7488aa12ebaf56185e66f5a10a4
Georgii Naumov <gmnaumov@yandex.ru>
2024-01-19 22:27:00 +0300

    feat: Добавлен пример включения кнопки в существующее меню

--- Solution.cs
+++ Solution.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using Intermech.Bars;
+﻿using Intermech.Bars;
 using Intermech.Interfaces.Plugins;
+using System;
+using System.Windows.Forms;
 
 namespace IpsSampleClientPlugin
 {
@@ -11,10 +12,21 @@ namespace IpsSampleClientPlugin
 
         internal static IServiceProvider ipsServiseProvider;
 
+
         public void Load(IServiceProvider serviceProvider)
         {
             ipsServiseProvider = serviceProvider;
 
+            AddNewMenuWithButtons();
+
+            AddNewButtonInExistsMenu();
+        }
+
+        /// <summary>
+        /// Пример добавления нового элемента главного меню IPS с кнопкой
+        /// </summary>
+        private void AddNewMenuWithButtons()
+        {
             BarManager barManager = ipsServiseProvider.GetService(typeof(BarManager)) as BarManager;
 
             // Получаем ссылку на главное меню IPS
@@ -32,6 +44,9 @@ namespace IpsSampleClientPlugin
             // Уникальное имя кнопки, по которому будет выполняться поиск 
             menuButton.CommandName = "MenuButtonSample";
 
+            // Подсказка при наведении на кнопку
+            menuButton.ToolTipText = "Эта кнопка добавлена при помощи клиентского плагина";
+
             // Добавляем кнопку в элемент меню
             menuItem.Items.Add(menuButton);
 
@@ -39,9 +54,47 @@ namespace IpsSampleClientPlugin
             menuBar.Items.Add(menuItem);
         }
 
+        /// <summary>
+        /// Пример добавления кнопки в существующее меню IPS, с обработчиком события нажатия на кнопку
+        /// </summary>
+        private void AddNewButtonInExistsMenu()
+        {
+            BarManager barManager = ipsServiseProvider.GetService(typeof(BarManager)) as BarManager;
+
+            MenuBar menuBar = barManager.MenuBar;
+
+            // Поиск существующего элемента меню по значению свойства CommandName
+            MenuItemBase menuItem = menuBar.FindMenuBar("MenuElementSample");
+
+            if (menuItem != null)
+            {
+                MessageBox.Show("Элемент меню найден!");
+
+                MenuButtonItem menuButton = new MenuButtonItem("Пример кнопки в меню #3",
+                    new EventHandler(ShowHelloMessage));
+
+                menuButton.CommandName = "MenuButtonSampleTwo";
+
+                menuItem.Items.Add(menuButton);
+            }
+            else
+            {
+                MessageBox.Show("Элемент меню не найден...");
+            }
+        }
+
+        /// <summary>
+        /// Метод интерфейса IPackage. 
+        /// В новых версиях IPS не используется, оставлен для совместимости.
+        /// </summary>
         public void Unload()
         {
-            throw new NotImplementedException();
+
+        }
+
+        private static void ShowHelloMessage(object sender, EventArgs e)
+        {
+            MessageBox.Show("Это сообщение вызвано обработчиком события IPS");
         }
     }
 }
